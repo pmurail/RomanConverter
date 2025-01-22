de160697a5248b314f5ca9941d1304700f7d9854
Pierre Murail <pierre.murail@itsf.io>
2025-01-22 13:52:03 +0100

    first commit

---
+++ RomanConverter/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RomanConverter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez un chiffre romain : ");
+            string roman = Console.ReadLine();
+
+            try
+            {
+                RomanValueIsWrong(roman.ToUpper());
+                int result = RomanToInt(roman);
+                Console.WriteLine($"La valeur entière de {roman} est : {result}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Erreur : {ex.Message}");
+            }
+        }
+
+        public static int RomanToInt(string romanValue)
+        {
+            Dictionary<char, int> romanValues = new Dictionary<char, int>
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 }
+            };
+
+            int total = 0;
+            int prevValue = 0;
+
+            for (int i = romanValue.Length - 1; i >= 0; i--)
+            {
+                char currentChar = romanValue.ToUpper()[i];
+                int currentValue = romanValues[currentChar];
+
+                if (currentValue < prevValue)
+                {
+                    total -= currentValue;
+                }
+                else
+                {
+                    total += currentValue;
+                }
+
+                prevValue = currentValue;
+            }
+
+            return total;
+        }
+
+        public static void RomanValueIsWrong(string romanValue)
+        {
+            if (!Regex.IsMatch(romanValue, "^[IVXLCDM]+$", RegexOptions.IgnoreCase))
+            {
+                throw new ArgumentException("Le chiffre romain contient des caractères invalides.");
+            }
+
+            if (Regex.IsMatch(romanValue, "IIII|XXXX|CCCC|MMMM"))
+            {
+                throw new ArgumentException("Le chiffre romain ne peut pas contenir plus de trois répétitions consécutives.");
+            }
+
+            if (Regex.IsMatch(romanValue, "(I{4,}|V{2,}|X{4,}|L{2,}|C{4,}|D{2,}|M{4,})"))
+            {
+                throw new ArgumentException("Le chiffre romain ne peut pas contenir des séquences répétées non valides.");
+            }
+
+            if (Regex.IsMatch(romanValue, "I[^IV]|V[^IX]|X[^XL]|L[^XC]|C[^CD]|D[^CM]|M[^M]"))
+            {
+                throw new ArgumentException("Le chiffre romain contient des séquences invalides.");
+            }
+        }
+    }
+}
---
+++ RomanConverterTest/UnitTest.cs
@@ -0,0 +1,33 @@
+﻿using RomanConverter;
+
+namespace RomanConverterTest
+{
+    public class UnitTest
+    {
+        [Fact] 
+        public void TestValidConversions()
+        {
+            Assert.Equal(1, Program.RomanToInt("I"));
+            Assert.Equal(4, Program.RomanToInt("IV"));
+            Assert.Equal(9, Program.RomanToInt("IX"));
+            Assert.Equal(58, Program.RomanToInt("LVIII"));
+            Assert.Equal(1994, Program.RomanToInt("MCMXCIV"));
+            Assert.Equal(3549, Program.RomanToInt("MMMDXLIX"));
+        }
+
+        [Fact] 
+        public void TestInvalidConversions()
+        {
+            Assert.Throws<ArgumentException>(() => Program.RomanValueIsWrong("IIII"));
+            Assert.Throws<ArgumentException>(() => Program.RomanValueIsWrong("XXXX"));
+            Assert.Throws<ArgumentException>(() => Program.RomanValueIsWrong("MMMM"));
+        }
+
+        [Fact] 
+        public void TestEdgeCases()
+        {
+            Assert.Throws<ArgumentException>(() => Program.RomanValueIsWrong("A"));
+            Assert.Throws<ArgumentException>(() => Program.RomanValueIsWrong(""));
+        }
+    }
+}
